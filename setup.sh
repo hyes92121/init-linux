af76da381d76a1fd18c2909b7b25cdc77b39eb14
Caleb <hyes92121@gmail.com>
2019-09-20 17:30:54 +0800

    Modify for linux env

---
+++ setup.sh
@@ -0,0 +1,40 @@
+cd .. 
+
+# Backup original user files
+need_backup=false
+mkdir .env_backup
+if [ -f .bashrc ]; then
+    echo .bashrc exists. Moving to backup...
+    mv .bashrc .env_backup
+    need_backup=true;
+fi 
+if [ -f .vimrc ]; then
+    echo .vimrc exists. Moving to backup...
+    mv .vimrc .env_backup
+    need_backup=true
+fi
+if [ -d .vim ]; then
+    echo .vim exists. Moving to backup...
+    mv .vim .env_backup
+    need_backup=true
+fi
+# Delete environment backup if not needed
+if [ "$need_backup" = false]; then
+    rm -rf .env_backup
+fi
+
+# move files to home directory
+mv ez-bash/.bash* .
+mv ez-bash/.vim* .
+rm -rf ez-bash
+
+source .bashrc
+echo Finish setting up bash and vim env. 
+echo Start downloading miniconda3 
+if [ -z $(which wget) ]; then 
+    echo wget not found. Installing wget...
+    sudo apt install wget
+fi 
+echo Done!
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh
